949a6dcc6174ed68cf06b68374f2f725303b9f04
Olivier Soulet <olivier.soulet@akeneo.com>
2015-12-19 15:48:41 +0100

    Add achievement entity #3

---
+++ src/Ironforge/AchievementBundle/Entity/Achievement.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace Ironforge\AchievementBundle\Entity;
+
+/**
+ * Achievement
+ */
+class Achievement
+{
+    /** @var int */
+    protected $id;
+
+    /** @var string */
+    protected $title;
+
+    /** @var string */
+    protected $description;
+
+    /** @var string */
+    protected $image;
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param string $title
+     *
+     * @return $this
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param string $description
+     *
+     * @return $this
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getImage()
+    {
+        return $this->image;
+    }
+
+    /**
+     * @param string $image
+     *
+     * @return $this
+     */
+    public function setImage($image)
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+}
