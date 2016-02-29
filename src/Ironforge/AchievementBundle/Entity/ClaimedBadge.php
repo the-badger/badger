98932070f8dc037d61dae833f7129497e645caca
Adrien Pétremann <petremann.adrien@gmail.com>
2016-02-29 22:15:58 +0100

    User can claim a badge

---
+++ src/Ironforge/AchievementBundle/Entity/ClaimedBadge.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace Ironforge\AchievementBundle\Entity;
+
+use Ironforge\UserBundle\Entity\User;
+
+/**
+ * An ClaimedBadge is the entity that represents the user wanting a badge.
+ *
+ * @author Adrien Pétremann <adrien.petremann@akeneo.com>
+ */
+class ClaimedBadge
+{
+    /** @var string */
+    protected $id;
+
+    /** @var User */
+    protected $user;
+
+    /** @var Badge */
+    protected $badge;
+
+    /** @var DateTime */
+    protected $claimedDate;
+
+    /**
+     * @return string
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param string $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return User
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    /**
+     * @param User $user
+     */
+    public function setUser($user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * @return Badge
+     */
+    public function getBadge()
+    {
+        return $this->badge;
+    }
+
+    /**
+     * @param Badge $badge
+     */
+    public function setBadge($badge)
+    {
+        $this->badge = $badge;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getClaimedDate()
+    {
+        return $this->claimedDate;
+    }
+
+    /**
+     * @param \DateTime $claimedDate
+     */
+    public function setClaimedDate($claimedDate)
+    {
+        $this->claimedDate = $claimedDate;
+    }
+}
--- src/Ironforge/AchievementBundle/Repository/ClaimedBadgeRepository.php
+++ src/Ironforge/AchievementBundle/Repository/ClaimedBadgeRepository.php
@@ -3,28 +3,27 @@
 namespace Ironforge\AchievementBundle\Repository;
 
 use Doctrine\ORM\EntityRepository;
-use Ironforge\AchievementBundle\Entity\Badge;
 use Ironforge\UserBundle\Entity\User;
 
 /**
- * @author Adrien Pétremann <petremann.adrien@gmail.com>
+ * @author Adrien Pétremann <adrien.petremann@akeneo.com>
  */
-class UnlockedBadgeRepository extends EntityRepository
+class ClaimedBadgeRepository extends EntityRepository
 {
     /**
-     * Get all Badge ids unlocked by the given $user.
+     * Get all Badge ids claimed by the given $user.
      *
      * @param User $user
      *
      * @return array
      */
-    public function getUnlockedBadgeIdsByUser(User $user)
+    public function getBadgeIdsClaimedByUser(User $user)
     {
         $qb = $this->getEntityManager()->createQueryBuilder();
         $qb->select('b.id')
-            ->from('AchievementBundle:UnlockedBadge', 'ub')
-            ->leftjoin('ub.badge', 'b')
-            ->where($qb->expr()->eq('ub.user', '?1'))
+            ->from('AchievementBundle:ClaimedBadge', 'cb')
+            ->leftjoin('cb.badge', 'b')
+            ->where($qb->expr()->eq('cb.user', '?1'))
             ->setParameter(1, $user);
 
         $queryResult = $qb->getQuery()->getScalarResult();
