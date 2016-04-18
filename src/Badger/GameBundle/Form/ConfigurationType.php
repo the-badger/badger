94f6a4435bc8137e9ff8283b64bac7f9107bb8be
skeleton <marie.bochu@akeneo.com>
2016-04-18 14:27:46 +0200

    Add crud for configuration

---
+++ src/Badger/GameBundle/Form/ConfigurationType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Badger\GameBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ConfigurationType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('code')
+            ->add('value', 'textarea')
+        ;
+    }
+
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => 'Badger\GameBundle\Entity\Configuration'
+        ]);
+    }
+}
