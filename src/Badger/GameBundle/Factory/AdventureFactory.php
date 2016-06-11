c6b481fad36249cde345ba4940942c82d334972e
skeleton <marie.bochu@akeneo.com>
2016-06-11 14:58:51 +0200

    Remove useless BadgeFactoryInterface

--- src/Badger/GameBundle/Factory/AdventureFactory.php
+++ src/Badger/GameBundle/Factory/AdventureFactory.php
@@ -10,7 +10,7 @@ use Badger\GameBundle\Entity\Step;
  * @copyright 2016 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
  */
-class AdventureFactory implements BadgeFactoryInterface
+class AdventureFactory implements SimpleFactoryInterface
 {
     /**
      * {@inheritdoc}
--- src/Badger/GameBundle/Factory/BadgeFactory.php
+++ src/Badger/GameBundle/Factory/BadgeFactory.php
@@ -10,7 +10,7 @@ use Badger\GameBundle\Entity\Badge;
  * @copyright 2016 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
  */
-class BadgeFactory implements BadgeFactoryInterface
+class BadgeFactory implements SimpleFactoryInterface
 {
     /**
      * {@inheritdoc}
