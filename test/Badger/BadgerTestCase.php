8ddda501a5a5448326dc01dafe0f9156ea63b1c3
Adrien Pétremann <hello@grena.fr>
2018-03-14 13:57:28 +0100

    Change PHPdoc to update license

--- test/Badger/BadgerTestCase.php
+++ test/Badger/BadgerTestCase.php
@@ -11,7 +11,7 @@ use Symfony\Component\DependencyInjection\Container;
 /**
  * @author    Marie Bochu <marie.bochu@akeneo.com>
  * @copyright 2016 Akeneo SAS (http://www.akeneo.com)
- * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @license   http://opensource.org/licenses/MIT The MIT License (MIT)
  */
 class BadgerTestCase extends WebTestCase
 {
